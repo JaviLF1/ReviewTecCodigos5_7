e1d33a48b8a495ad70254e47a5c699ff9ad70f4e
JaviLF1 <javierlopez25177152@gmail.com>
2026-09-06 22:55:58 -0600

    Terminal compiled code

---
+++ R1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    // Alternatively in modern C++23/C++26:
+    // std::println("Hello, World!");
+    return 0;
+}
